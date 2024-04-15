a6e6bd862e4a32a29b47b7758598232d8d5f77ca
takayoshi56 <162421510+takayoshi56@users.noreply.github.com>
2024-04-15 15:20:22 +0900

    Delete kadai_020 directory

--- kadai_020/event.js
+++
@@ -1,9 +0,0 @@
-const btn=document.getElementById('btn');
-const text=document.getElementById('text');
-
-btn.addEventListener('click',()=>{
-  const childList=document.createElement('text');
-  childList.textContent='ボタンをクリックしました';
-  text.textContent="ボタンをクリックしました";
-  console.log(text);
-})
